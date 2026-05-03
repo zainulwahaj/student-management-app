43c565bdae4654b79e1df44b0776ac341f74bf31
zainulwahaj <zainulwahaj@gmail.com>
2026-05-03 17:38:56 +0500

    fix

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -35,4 +35,4 @@ print("    Schema ready.")
 PY
 
 echo "==> Starting Gunicorn on :5000"
-exec gunicorn --bind 0.0.0.0:5000 --workers 2 --timeout 60 --access-logfile - app:app
+exec gunicorn --bind 0.0.0.0:5000 --workers 4 --timeout 60 --access-logfile - app:app
